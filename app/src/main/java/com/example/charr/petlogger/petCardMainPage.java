66e93df249975387df1ac07c9ffb8726c5eefe13
Charrisse Porter <charrissevporter@gmail.com>
2018-10-23 13:10:43 -0700

    Initial commit

---
+++ app/src/main/java/com/example/charr/petlogger/petCardMainPage.java
@@ -0,0 +1,13 @@
+package com.example.charr.petlogger;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class petCardMainPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pet_card_main_page);
+    }
+}
