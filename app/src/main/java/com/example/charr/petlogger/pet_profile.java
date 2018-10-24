bc5c27957f99b6ad78079633a3d248535818d6e8
jesuschav <jesus.chavez886@myci.csuci.edu>
2018-10-24 01:22:22 -0700

    -Added Pet Profile page   - pet_profile.java is just waiting to get connected to weightlog and main activity

---
+++ app/src/main/java/com/example/charr/petlogger/pet_profile.java
@@ -0,0 +1,44 @@
+package com.example.charr.petlogger;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
+
+public class pet_profile extends AppCompatActivity {
+    private Button backButton;
+    private Button weightLogButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pet_profile);
+
+        backButton = (Button) findViewById(R.id.button2);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMainPage();
+            }
+        });
+
+        weightLogButton= (Button) findViewById(R.id.button3);
+        weightLogButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openWeightLog();
+            }
+        });
+    }
+
+    public void openMainPage(){
+        Intent intent = new Intent(this, petCardMainPage.class);
+        startActivity(intent);
+    }
+
+    public void openWeightLog(){
+        Intent intent = new Intent(this, petCardMainPage.class);
+        startActivity(intent);
+    }
+}
