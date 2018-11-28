c14e50de25d3386ddac7575e58b8de7072aee954
Charrisse Porter <charrissevporter@gmail.com>
2018-11-28 13:56:44 -0800

    Imported GraphView working on adding data to graph

--- app/src/main/java/com/example/charr/petlogger/card_item.java
+++ app/src/main/java/com/example/charr/petlogger/card_item.java
@@ -1,6 +1,16 @@
 package com.example.charr.petlogger;
 
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.helper.DateAsXAxisLabelFormatter;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
+
+import java.util.Calendar;
+import java.util.Date;
 
 public class card_item extends AppCompatActivity {
     private int mImageResource;
@@ -8,13 +18,22 @@ public class card_item extends AppCompatActivity {
     private String mMorph;
     private String mSex;
     private String mBday;
+    private double mInitialWeight;
+    private Calendar mCalendar = Calendar.getInstance();
+    private GraphView mGraph = (GraphView) findViewById(R.id.weight_graph);
+    private LineGraphSeries<DataPoint> mWeights;
+    private weight_log weightLog = new weight_log();
 
-    public card_item(int imageResource, String name, String morph, String sex)
+    public card_item(int imageResource, String name, String morph, String sex, double mInitialWeight)
     {
         mImageResource = imageResource;
         mName = name;
         mMorph = morph;
         mSex = sex;
+
+
+        // Sets up graph and adds the initial weight
+        formatGraph(mInitialWeight);
     }
 
     public int getImageResource() {return mImageResource;}
@@ -26,5 +45,28 @@ public class card_item extends AppCompatActivity {
     public String getSex()
     {
         return mSex;
+    }
+
+    public double getInitialWeight() {return mInitialWeight;}
+
+    public Calendar getCalendar() {return mCalendar;}
+
+    public GraphView getGraph() {return mGraph;}
+
+    public LineGraphSeries<DataPoint> getWeights() {return mWeights;}
+
+    private void formatGraph(double initialWeight)
+    {
+        // Initial weight
+        Date mCurrentDate = mCalendar.getTime();
+        mWeights = new LineGraphSeries<>(new DataPoint[] {new DataPoint(mCurrentDate, mInitialWeight)});
+        mGraph.addSeries(mWeights);
+        mGraph.getGridLabelRenderer().setLabelFormatter(new DateAsXAxisLabelFormatter());
+        mGraph.getGridLabelRenderer().setNumHorizontalLabels(3);
+
+        mGraph.getViewport().setMinX(mCurrentDate.getTime());
+        mGraph.getViewport().setXAxisBoundsManual(true);
+
+        mGraph.getGridLabelRenderer().setHumanRounding(false);
     }
 }
